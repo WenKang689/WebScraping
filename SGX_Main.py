41795b7e87da20d20ba4ea5db082f40e705bb430
Wen Kang <work.cwk689@gmail.com>
2025-03-04 19:45:06 +0800

    Base code done

--- SGX_Main.py
+++ SGX_Main.py
@@ -4,6 +4,7 @@ import os
 import logging
 import requests
 from datetime import datetime, timedelta
+import time
 
 #Load configurations in config.ini
 config_file = "config.ini"
@@ -15,41 +16,25 @@ dir = config.get("Settings", "download_directory", fallback = "./downloads")
 retry_duration = config.getint("Settings", "retry_duration", fallback = 3)
 log_file = config.get("Settings", "log_file", fallback = "download.log")
 
-#Name different folders for each data type
-folder_directory = {
-    "WEBPXTICK_DT": os.path.join(dir, "WEBPXTICK_DT"),
-    "TickData_structure": os.path.join(dir, "TickData_structure"),
-    "TC": os.path.join(dir, "TC"),
-    "TC_structure": os.path.join(dir, "TC_structure"),
-}
-
-#Create new folders if folder not exists
-for folder in folder_directory.values():
-    os.makedirs(folder, exist_ok=True)
+#Ensure the folder is created
+os.makedirs(dir, exist_ok=True)
 
 #Configure logging
 logging.basicConfig(
     filename=log_file,
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
+    format="%(asctime)s - %(levelname)s - %(message)s",)
 
 #Display log message in console
 handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
 logging.getLogger().addHandler(handler)
 
-# Get save path for each file type
-def get_save_path(file_name):
-    if file_name == ("WEBPXTICK_DT.zip"):
-        return os.path.join(folder_directory["WEBPXTICK_DT"], file_name)
-    elif file_name == "TickData_structure.dat":
-        return os.path.join(folder_directory["TickData_structure"], file_name)
-    elif file_name == ("TC.txt"):
-        return os.path.join(folder_directory["TC"], file_name)
-    elif file_name == "TC_structure.dat":
-        return os.path.join(folder_directory["TC_structure"], file_name)
-    return os.path.join(dir, file_name)
+# Create folder and get save path for each index
+def get_save_path(index):
+    index_folder = os.path.join(dir,str(index))
+    os.makedirs(index_folder, exist_ok=True)
+    return index_folder
 
 #Calculate the corresponding index
 def index_calc(dates):
@@ -77,21 +62,51 @@ def index_calc(dates):
 
 #Download data
 def dwl_data(indices):
-
     file_name = ["WEBPXTICK_DT.zip","TickData_structure.dat","TC.txt","TC_structure.dat"]
     link = config.get("Settings","SGX_URL",fallback="https://links.sgx.com/1.0.0/derivatives-historical/")
+
     for index in indices:
         for file in file_name:
             base_url = f"{link}{index}/{file}"
-            response = requests.get(base_url)
-            if response == 200:
-                path = get_save_path(file)
-                with open(path,"wb") as w:
-                    w.write(response.content)
-                logging.info(f"Downloaded")
-            else:
-                print(f"Download failed")
+            path = get_save_path(index)
+            save_path = os.path.join(path,file)
 
+            if os.path.exists(save_path):
+                continue
+
+            try:
+                response = requests.get(base_url)
+                if response.status_code == 200:
+                    with open(save_path,"wb") as w:
+                        w.write(response.content)
+                    logging.info(f"Downloaded: {os.path.basename(file)} (Index: {index})")
+                else:
+                    dwl_retry(index,file)
+                    logging.warning(f"Download failed for {os.path.basename(file)} (Index: {index}). HTTP {response.status_code}")
+            except requests.RequestException as e:
+                dwl_retry(index,file)
+                logging.error(f"Error downloading {file} for index {index}: {e}")
+
+def dwl_retry(index,file):
+    link = config.get("Settings","SGX_URL",fallback="https://links.sgx.com/1.0.0/derivatives-historical/")
+    logging.info(f"Retrying failed downloads after {retry_duration} hours...")
+    time.sleep(retry_duration * 3600)    
+
+    for index, file in fail_dwl:
+        base_url = f"{link}{index}/{file}"
+        path = get_save_path(index)
+        save_path = os.path.join(path,file)
+
+        try:
+            response = requests.get(base_url)
+            if response.status_code == 200:
+                with open(save_path, "wb") as w:
+                    w.write(response.content)
+                logging.info(f"Download retry successful: {os.path.basename(file)} (Index: {index})")
+            else:
+                logging.warning(f"Download retry failed for {os.path.basename(file)} (Index: {index}). HTTP {response.status_code}")
+        except requests.RequestException as e:
+            logging.error(f"Download retry error: {file} for index {index}: {e}")
 
 #---Main---
 
@@ -105,7 +120,7 @@ arg = argparse.ArgumentParser.parse_args()
 # Calculate date range for download
 dates = []
 if arg.retry:
-    retry_download()
+    dwl_retry(index,file)
     exit(0)
 
 if arg.date:
