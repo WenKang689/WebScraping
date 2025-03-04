d4711925d23452b4398b01a776285dd9bd38100f
Wen Kang <work.cwk689@gmail.com>
2025-03-05 00:00:41 +0800

    Code done wait for test

--- SGX_Main.py
+++ SGX_Main.py
@@ -13,17 +13,24 @@ config.read(config_file)
 
 #Read settings from config.ini
 dir = config.get("Settings", "download_directory", fallback = "./downloads")
-retry_duration = config.getint("Settings", "retry_duration", fallback = 3)
+link = config.get("Settings","SGX_URL",fallback="https://links.sgx.com/1.0.0/derivatives-historical/")
 log_file = config.get("Settings", "log_file", fallback = "download.log")
+retry_cooldown = config.getint("Settings", "retry_cooldown", fallback = 3)
+max_retry = config.getint("Settings", "max_retries", fallback=3)
+base_date = datetime.strptime(config.get("Settings", "base_date", fallback = "2021-01-01"),"%Y-%m-%d")
+base_index = int(config.get("Settings","base_index",fallback = "4803"))
+files = ["WEBPXTICK_DT.zip","TickData_structure.dat","TC.txt","TC_structure.dat"]
+failed_download_log = "Failed_Download.log"
 
 #Ensure the folder is created
 os.makedirs(dir, exist_ok=True)
+open(failed_download_log, 'a').close()
 
 #Configure logging
 logging.basicConfig(
-    filename=log_file,
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",)
+    filename = log_file,
+    level = logging.INFO,
+    format = "%(asctime)s - %(levelname)s - %(message)s",)
 
 #Display log message in console
 handler = logging.StreamHandler()
@@ -38,17 +45,13 @@ def get_save_path(index):
 
 #Calculate the corresponding index
 def index_calc(dates):
-    base_date = datetime.strptime(config.get("Settings", "base_date", fallback = "2021-01-01"),"%Y-%m-%d")
-    base_index = int(config.get("Settings","base_index",fallback = "4803"))
-
     date_now = base_date
     index_now = base_index
 
     index_list = []
     for date in dates:
         if date < base_date:
-            index_list.append(None)
-            print(f"Warning: {date.strftime('%Y-%m-%d')} is before base date ({base_date.strftime('%Y-%m-%d')}).")
+            logging.warning(f"Warning: {date.strftime('%Y-%m-%d')} is before base date ({base_date.strftime('%Y-%m-%d')}).")
             continue
         
         while date_now < date:
@@ -56,77 +59,94 @@ def index_calc(dates):
                 index_now += 1
             date_now += timedelta(days = 1)
         index_list.append(index_now)
-    if len(dates) == 1:
-        return(index_list[0])
+
     return(index_list)
 
 #Download data
-def dwl_data(indices):
-    file_name = ["WEBPXTICK_DT.zip","TickData_structure.dat","TC.txt","TC_structure.dat"]
-    link = config.get("Settings","SGX_URL",fallback="https://links.sgx.com/1.0.0/derivatives-historical/")
+def dwl_data(indices,file_name):
     failed_dwl = []
 
     for index in indices:
         for file in file_name:
+            logging.info(f"Initializing download for file {file} (Index: {index})")
             base_url = f"{link}{index}/{file}"
             path = get_save_path(index)
             save_path = os.path.join(path,file)
 
             if os.path.exists(save_path):
+                logging.info(f"File {file} (Index: {index}) already exists.")
                 continue
 
             try:
+                logging.info(f"Requesting download file {file} (Index: {index})")
                 response = requests.get(base_url)
                 if response.status_code == 200:
                     with open(save_path,"wb") as w:
                         w.write(response.content)
                     logging.info(f"Downloaded: {os.path.basename(file)} (Index: {index})")
                 else:
-                    failed_dwl.append((index,file))
-                    logging.warning(f"Download failed for {os.path.basename(file)} (Index: {index}). HTTP {response.status_code}")
+                    failed_dwl.append((index,file,1))
+                    logging.warning(f"Failed downloading for {os.path.basename(file)} (Index: {index}). HTTP {response.status_code}")
             except requests.RequestException as e:
-                failed_dwl.append((index,file))
+                failed_dwl.append((index,file,1))
                 logging.error(f"Error downloading {file} for index {index}: {e}")
     
     if failed_dwl:
-        logging.info(f"Retrying failed download(s) after {retry_duration} hours.")
-        time.sleep(retry_duration * 3600)   
+        dwl_retry(failed_dwl)
 
-        for r_index,r_file in failed_dwl:
-            dwl_retry(r_index,r_file)
-
-def dwl_retry(index,file):
-    link = config.get("Settings","SGX_URL",fallback="https://links.sgx.com/1.0.0/derivatives-historical/")
+def dwl_retry(failed_dwl):
     logging.info(f"Retrying failed download(s)")
-    base_url = f"{link}{index}/{file}"
-    path = get_save_path(index)
-    save_path = os.path.join(path,file)
+    while failed_dwl:
+        logging.info(f"Waiting {retry_cooldown} hours before retrying failed downloads.")
+        time.sleep(retry_cooldown * 3600)
 
-    try:
-        response = requests.get(base_url)
-        if response.status_code == 200:
-            with open(save_path, "wb") as w:
-                w.write(response.content)
-            logging.info(f"Download retry successful: {os.path.basename(file)} (Index: {index})")
-        else:
-            logging.warning(f"Download retry failed for {os.path.basename(file)} (Index: {index}). HTTP {response.status_code}")
-    except requests.RequestException as e:
-        logging.error(f"Download retry error: {file} for index {index}: {e}")
+        buffer_queue = []
+
+        for index,file,attempt in failed_dwl:
+            if attempt > max_retry:
+                logging.error(f"Max retry attempts reached for {file} (Index {index})")
+                with open(failed_download_log,"a") as fail_log:
+                    fail_log.write(f"{datetime.now()} - Failed Download for {attempt} attempt: {file} (Index: {index})\n")
+                continue
+
+            logging.info(f"Retrying download attempt {attempt} for {file} (Index: {index})")
+            base_url = f"{link}{index}/{file}"
+            path = get_save_path(index)
+            save_path = os.path.join(path,file)
+
+            try:
+                response = requests.get(base_url)
+                if response.status_code == 200:
+                    with open(save_path, "wb") as w:
+                        w.write(response.content)
+                    logging.info(f"Download retry successful: {os.path.basename(file)} (Index: {index})")
+                else:
+                    buffer_queue.append((index,file,attempt+1))
+                    logging.warning(f"Download retry failed for {os.path.basename(file)} (Index: {index}). HTTP {response.status_code}")
+            except requests.RequestException as e:
+                buffer_queue.append((index,file,attempt+1))
+                logging.error(f"Download retry error: {file} for index {index}: {e}")
+                
+        failed_dwl = buffer_queue
 
 #---Main---
 
 #Parsing argument for manual intervention
-argparse.ArgumentParser.add_argument("--date", type=str)
-argparse.ArgumentParser.add_argument("--start", type=str)
-argparse.ArgumentParser.add_argument("--end", type=str)
-argparse.ArgumentParser.add_argument("--retry", action="store_true")
-arg = argparse.ArgumentParser.parse_args()
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("--date", type=str)
+arg_parser.add_argument("--start", type=str)
+arg_parser.add_argument("--end", type=str)
+arg_parser.add_argument("--file", nargs="+")
+
+arg = arg_parser.parse_args()
 
 # Calculate date range for download
 dates = []
-if arg.retry:
-    dwl_retry(index,file)
-    exit(0)
+
+if arg.file:
+    file = arg.file
+else:
+    file = files
 
 if arg.date:
     dates.append(datetime.strptime(arg.date, "%Y-%m-%d"))
@@ -134,13 +154,14 @@ elif arg.start and arg.end:
     start_date = datetime.strptime(arg.start, "%Y-%m-%d")
     end_date = datetime.strptime(arg.end, "%Y-%m-%d")
     for i in range((end_date - start_date).days + 1):
-        dates = [start_date + timedelta(days=i)]
+        dates.append(start_date + timedelta(days=i))
 elif arg.start:
     start_date = datetime.strptime(arg.start, "%Y-%m-%d")
     for i in range((datetime.today() - start_date).days + 1):
-        dates = [start_date + timedelta(days=i)]
+        dates.append(start_date + timedelta(days=i))
 else:
     dates.append(datetime.today())
 
 indices = index_calc(dates)
-dwl_data(indices)
+
+dwl_data(indices,file)
